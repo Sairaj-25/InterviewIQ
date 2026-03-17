f076f786775558ea3073bfabd43cf69dceb5a70e
Sairaj Jadhav <jadhavsairaj25@gmail.com>
2026-03-17 23:18:05 +0530

    fix: update main.py to serve frontend

--- speechfix/main.py
+++ speechfix/main.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pathlib import Path
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
+app = FastAPI(title="SpeechFix API")
 
+BASE_DIR = Path(__file__).resolve().parent
+# Mount the static directory
+app.mount(
+    "/speechfix/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static"
+)
+
+# setup templates directory
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+
+# root endpoint to return the html template
 @app.get("/")
-
-def home():
-    return {"message": "Hello World"}
-
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "score": 0})
